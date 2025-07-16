cf4d099ce48092f8f8b1e3b734e5cde606f604d2
David Pachinik <ing.pachinik96@gmail.com>
2025-07-16 19:18:17 -0400

    feat(ui): implement responsive  Horario table layout for class

---
+++ src/Components/Horarios/Horarios.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+
+const Horarios = () => {
+  const baseBorder = 'border-4 border-[#1C1C1C]';
+  const headerCellStyle = `${baseBorder} p-3 font-exo`;
+  const hourCellStyle = `${baseBorder} text-white font-light text-md p-4 whitespace-nowrap`;
+  const classCellStyle = `${baseBorder} font-normal px-2`;
+
+  return (
+    <div className="w-full">
+      <h3 className="mb-6 text-center font-exo text-[4vh] font-semibold text-white">
+        Clases y Horarios
+      </h3>
+      <div className="container mx-auto flex overflow-x-auto md:justify-center">
+        <table
+          className={`${baseBorder}text-xs min-w-[700px] bg-black font-medium text-gray-400 md:text-base`}
+        >
+          <thead className={baseBorder}>
+            <tr className={`${baseBorder} text-[#ff2323]`}>
+              <th className={headerCellStyle}>Horarios</th>
+              <th className={headerCellStyle}>Lunes</th>
+              <th className={headerCellStyle}>Martes</th>
+              <th className={headerCellStyle}>Miércoles</th>
+              <th className={headerCellStyle}>Jueves</th>
+              <th className={headerCellStyle}>Viernes</th>
+              <th className={headerCellStyle}>Sábado</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr className="text-center">
+              <td className={hourCellStyle}>05:00 - 12:00</td>
+              <td colSpan={5} className={classCellStyle}>
+                Musculación
+              </td>
+              <td className={baseBorder}></td>
+            </tr>
+
+            <tr className="text-center">
+              <td className={hourCellStyle}>12:00 - 19:00</td>
+              <td colSpan={5} className={classCellStyle}>
+                Musculación
+              </td>
+              <td className={baseBorder}></td>
+            </tr>
+
+            <tr className="text-center">
+              <td className={hourCellStyle}>14:00 - 18:00</td>
+              <td colSpan={5} className={baseBorder}></td>
+              <td className={classCellStyle}>Musculación</td>
+            </tr>
+
+            <tr className="text-center">
+              <td className={hourCellStyle}>19:00 - 20:00</td>
+              <td className={classCellStyle}>Musc. + Funcional</td>
+              <td className={classCellStyle}>Musc.</td>
+              <td className={classCellStyle}>Musc. + Funcional</td>
+              <td className={classCellStyle}>Musc.</td>
+              <td className={classCellStyle}>Musc. + Funcional</td>
+              <td className={classCellStyle}></td>
+            </tr>
+
+            <tr className="text-center">
+              <td className={hourCellStyle}>20:00 - 21:00</td>
+              <td className={classCellStyle}>Musc. + Cardio</td>
+              <td className={classCellStyle}>Musc.</td>
+              <td className={classCellStyle}>Musc. + Cardio</td>
+              <td className={classCellStyle}>Musc.</td>
+              <td className={classCellStyle}>Musc. + Cardio</td>
+              <td className={classCellStyle}></td>
+            </tr>
+
+            <tr className="text-center">
+              <td className={hourCellStyle}>21:00 - 22:00</td>
+              <td colSpan={5} className={classCellStyle}>
+                Musculación
+              </td>
+              <td className={classCellStyle}></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Horarios;
