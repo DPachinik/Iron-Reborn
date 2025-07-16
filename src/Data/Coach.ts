aec08b56475f5a98e2405dc3d8c35620dd86d812
David Pachinik <ing.pachinik96@gmail.com>
2025-07-16 19:00:58 -0400

    feat(ui): add PersonalCards component to class page

---
+++ src/Data/Coach.ts
@@ -0,0 +1,47 @@
+import gustavo2 from '../../public/gustavo2.png';
+import gustavo from '../../public/gus2.png';
+import ramiro from '../../public/ramiro2.png';
+import sergio from '../../public/sergio2.png';
+
+export type CoachTypes = {
+  name: string;
+  image: string;
+  studies1: string;
+  studies2: string;
+  studies3: string;
+  studies4?: string;
+  studies5?: string;
+};
+
+export const Coach: CoachTypes[] = [
+  {
+    name: 'SERGIO GONZÁLEZ',
+    image: sergio,
+    studies1: 'Especialista en Fisiculturismo y Fitness',
+    studies2: 'Dipl. en Nutrición Deportiva (Francis Holway)',
+    studies3: 'Dipl. en Fisiología del Ejercicio',
+    studies4: 'Dipl. en Ciencias del Ejercicio Nivel I ',
+    studies5: 'Antropometrista ISAK Nivel I',
+  },
+  {
+    name: 'RAMIRO FOIS HIRSCH',
+    image: ramiro,
+    studies1: 'Licenciado en Ciencias del Deporte',
+    studies2: 'Esp. en Entrenamiento físico para adultos mayores',
+    studies3: 'Personal Trainer',
+  },
+  {
+    name: 'GUSTAVO GONZÁLEZ',
+    image: gustavo2,
+    studies1: 'Coach Nutricional y Fitness',
+    studies2: 'Psicólogo Clínico y Deportivo',
+    studies3: 'Antropometrista ISAK Nivel I',
+  },
+  {
+    name: 'GUSTAVO VERDÚN',
+    image: gustavo,
+    studies1: 'Prof. de Entrenamiento Funcional',
+    studies2: 'Prof. de Spinning',
+    studies3: 'Personal Trainer',
+  },
+];
