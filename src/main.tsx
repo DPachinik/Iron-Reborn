4794767ada7caafdb9c58d0a125ba79efdbc0f48
David Pachinik <ing.pachinik96@gmail.com>
2026-01-26 17:00:21 -0300

    feat:add toastify

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
+      <ToastContainer autoClose={1500} />
       <App />
     </StrictMode>
   </BrowserRouter>
